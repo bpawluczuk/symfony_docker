72cb9ce4c9c571d16730b08f53030dac7ed074bb
bpawluczuk <b.pawluczuk@companion.pl>
2020-02-27 21:30:11 +0100

    init

--- sc3/config/bundles.php
+++ sc3/config/bundles.php
@@ -5,4 +5,5 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     SymfonyBundles\RedisBundle\SymfonyBundlesRedisBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
