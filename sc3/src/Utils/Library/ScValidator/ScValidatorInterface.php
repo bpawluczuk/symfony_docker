28c38c5f11d6685cb0875abf31e4835c14dce88c
bpawluczuk <b.pawluczuk@companion.pl>
2020-03-30 14:51:59 +0200

    init

--- sc3/src/Utils/Library/ScValidator/ScValidatorInterface.php
+++ sc3/src/Utils/Library/ScValidator/ScValidatorInterface.php
@@ -5,11 +5,15 @@
 
 namespace App\Utils\Library\ScValidator;
 
+use Symfony\Component\Validator\Constraint;
+
 /**
  * Interface ScValidatorInterface
  * @package App\Utils\Library\ScValidator
  */
 interface ScValidatorInterface
 {
+    public function propertyValidate($property, Constraint $constraint): array;
     public function entityValidate(object $object): array;
+    public function arrayValidate(array $fields, array $constraints): array;
 }
